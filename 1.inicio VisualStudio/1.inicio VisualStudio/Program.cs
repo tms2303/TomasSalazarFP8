7263e698c0b5a9f09d8d40f05593d14042299426
Tomas Salazar <tomas.salazara.col@upb.edu.co>
2025-08-01 09:29:25 -0500

    variables, tipos de datos y constantes

--- 1.inicio VisualStudio/1.inicio VisualStudio/Program.cs
+++ 1.inicio VisualStudio/1.inicio VisualStudio/Program.cs
@@ -10,6 +10,15 @@ namespace _1.inicio_VisualStudio
     {
         static void Main(string[] args)
         {
+            //Declaracion e inicializacion de variables
+            string nombre ="Tomas";
+            const string iva = "19%";
+            string apellidos = "Salazar Arroyave";
+            Console.Write(nombre+" Bienvenido al curso de fundamentos programacion\n");
+            nombre = "Roman";
+            //iva="20%"; no se puede cambiar el valor por ser una constante
+            Console.Write("\t" + nombre + "" + apellidos + "" + iva);
+            Console.ReadKey();
         }
     }
 }
