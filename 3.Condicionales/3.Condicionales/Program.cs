d8e2d53bb6fa766f026a52dd21823c76d451086e
tms2303 <tomas.salazara.col@upb.edu.co>
2025-08-08 09:19:22 -0500

    condicionales
    condicionales y queda un ejercicio pendiente

---
+++ 3.Condicionales/3.Condicionales/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _3.Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos.
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre = Console.ReadLine(); //todo lo que se lee dentro de la consola llega como un string
+
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+            if (sueldo>3000)
+            {
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+
+            }
+            //Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”. 
+
+        }
+    }
+}
