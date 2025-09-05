cb97639f10c242032243e6dddfd8cd5ae88038a6
tms2303 <tomas.salazara.col@upb.edu.co>
2025-09-05 09:29:40 -0500

    DoWhile

---
+++ 17.DoWhile/17.DoWhile/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _17.DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //algoritmo que genere y calcule la suma de los primeros 5 numerods enteros positivos
+
+            /*int contador = 0;
+            int acumulador = 0;
+
+            do
+            {
+                contador++;
+                acumulador += contador;
+            } while (contador <= 5);
+
+            Console.WriteLine($"la suma de los cinco numeros es: {acumulador}");*/
+
+            /*Algoritmo que solicita un número y genere su correspondiente tabla de multiplicar desde el 
+             * 1 hasta el 10. Y así sucesivamente hasta que el usuario ya no desee continuar generando tablas de multiplicar. */
+
+            int numero = 0;
+            int multiplicacion = 1;
+            string respuesta = " ";
+
+            do
+            {
+
+                Console.WriteLine("Por favor ingrese un numero");
+                numero = Int32.Parse((Console.ReadLine()));
+
+                do
+                {
+                    Console.WriteLine($"{numero} X {multiplicacion} = {numero * multiplicacion}");
+                    multiplicacion++;
+                } while (multiplicacion <= 10);
+
+                multiplicacion = 1;
+                Console.WriteLine("Quiere generar otra tabla de multiplicacion: s:si - n:no");
+                respuesta = Console.ReadLine().ToLower();
+
+            } while (respuesta == "s");
+        }
+    }
+}
---
+++ 18.DoWhile/18.DoWhile/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _18.DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          /*Algoritmo que le pida a un grupo de usuarios su nombre, número de cuenta y saldo; para cada usuario se debe hacer lo siguiente: 
+          Si el saldo es mayor a 3’000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y “Es apto para el crédito” 
+          Si el saldo es menor a 3’000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y “No es apto para el crédito” 
+          El algoritmo debe permitir preguntar la información de usuarios, hasta que se le indique que ya no se desea preguntar más. 
+          Además, el algoritmo debe permitir mostrar el número de usuarios a los que se le pregunto por la información, y debe mostrar el promedio de los saldos. */
+
+        }
+    }
+}
