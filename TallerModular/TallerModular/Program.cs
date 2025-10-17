6968bd0d67759cbadb2e52192cb773f13884f981
tms2303 <tomas.salazara.col@upb.edu.co>
2025-10-17 09:07:55 -0500

    Add files via upload

---
+++ TallerModular/TallerModular/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace TallerModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Desarrolla un programa que permita: 
+              Ingresar una palabra 
+              LLevar la palabra ingresada a un vector de caracteres 
+              Contar la cantidad de vocales y consonantes en la palabra proporcionada por el 
+             usuario.*/
+
+/* string palabra;
+ string[] caracteres;
+ int vocales = 0;
+
+ Console.WriteLine("Ingrese una palabra:");
+ palabra = Console.ReadLine().ToLower();
+
+ caracteres = new string[palabra.Length];
+
+ for(int i = 0; i<palabra.Length; i++)
+ {
+     caracteres[i] = palabra[i].ToString();                
+ }
+
+ Console.WriteLine();
+
+ for(int i =0; i<caracteres.Length; i++)
+ {
+     if (caracteres[i] == "a" || caracteres[i] == "e" || caracteres[i] == "i" || caracteres[i] == "o" || caracteres[i] == "u")
+     {
+         vocales++;
+     }
+ }
+
+ Console.WriteLine($"La palabra: {palabra} tiene {vocales} vocales y {caracteres.Length - vocales} consonantes.");*/
+
+           /*Crear un programa que permita gestionar una lista de tareas:  
+            Agregar tareas.  
+            Eliminar tareas  
+            Mostrar la lista de tareas.*/
+
+        }
+    }
+}
